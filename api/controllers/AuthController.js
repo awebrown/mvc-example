3a023b45a0760d86a25e25fdc33d6a6e6e012e3a
Brian Brown <brian@uzurv.com>
2016-05-31 15:46:01 -0400

    fixed register method to log user in

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -10,14 +10,24 @@
 
 module.exports = require('waterlock').waterlocked({
   register: function(req, res) {
-    var params = req.params.all(),
+    var params = waterlock._utils.allParams(req),
       def = waterlock.Auth.definition,
       criteria = { },
       scopeKey = def.email !== undefined ? 'email' : 'username';
 
+    // If there is only 1 chosen auth method just assume it
+    if(waterlock._utils.countTopLevel(waterlock.methods) === 1){
+      params.type = waterlock._utils.accessObjectLikeArray(0, waterlock.methods).authType;
+    }
+
+    if(typeof params.type === 'undefined'){
+      return res.badRequest('You must specify a type parameter.');
+    }
+
     var attr = {
       password: params.password
     };
+
     attr[scopeKey] = params[scopeKey];
     criteria[scopeKey] = attr[scopeKey];
 
@@ -26,10 +36,15 @@ module.exports = require('waterlock').waterlocked({
         return res.badRequest("User already exists");
       else
         waterlock.engine.findOrCreateAuth(criteria, attr, function(err, user) {
-          if (err)
+          if (err) {
             return res.badRequest(err);
-          delete user.password;
-          return res.ok(user);
+          }
+          if(waterlock.methods.hasOwnProperty(params.type)){
+            // call the login function of the correct auth type
+            waterlock.methods[params.type].actions.login(req, res);
+          }else{
+            return res.badRequest('unknown/invalid authentication type');
+          }
         });
     });
   }
