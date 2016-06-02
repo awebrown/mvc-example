a8b0a234cdff33cd6a8f24a53a126529dfe678a4
Anthony Brown <awebrown@gmail.com>
2016-06-02 08:49:08 -0400

    fileupload directive

--- assets/components/profile/profileController.js
+++ assets/components/profile/profileController.js
@@ -39,9 +39,14 @@ angular.module('dating-site.controllers')
         restrict: 'E',
         templateUrl: 'directives/profilenav.html'
       }
+    })
+
+  .directive("fileUpload", function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'directives/fileUpload.html'
+      }
     });
-
-
 
 
 
